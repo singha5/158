1f3ffa19dd8bcda62d03fe641f9e17aae1889697
singha5 <arunjitsingh6@gmail.com>
2014-03-26 10:06:31 -0400

    157

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Write(" Enter a Vowel: ");
+                String input = Console.ReadLine(). ToLower();
+                if(input == "a"|| input == "e"|| input == "i"|| input == "o"|| input == "u")
+                {
+                    Console.WriteLine("Ok");
+                    Console.WriteLine();
+                }
+                else
+
+                Console.WriteLine
+            }
+        }
+    }
+}
